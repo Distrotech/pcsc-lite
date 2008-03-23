e22b3109150262c57e8a3957d8767f648033a06d
rousseau <rousseau@0ce88b0d-b2fd-0310-8134-9614164e65ea>
2008-03-23 13:40:04 +0000

    release 1.4.100
    git-svn-id: svn://anonscm.debian.org/pcsclite/tags/PCSC@2853 0ce88b0d-b2fd-0310-8134-9614164e65ea

---
+++ rel-1.4.100/src/configfile.h
@@ -0,0 +1,26 @@
+/*
+ * Header file for reading lexical config files.
+ *
+ * MUSCLE SmartCard Development ( http://www.linuxnet.com )
+ *
+ * Copyright (C) 1999
+ *  David Corcoran <corcoran@linuxnet.com>
+ *
+ * $Id$
+ */
+
+#ifndef __configfile_h__
+#define __configfile_h__
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+	int DBGetReaderList(const char *readerconf, SerialReader **caller_reader_list);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif							/* __configfile_h__ */
---
+++ rel-1.4.100/src/eventhandler.h
@@ -0,0 +1,53 @@
+/*
+ * MUSCLE SmartCard Development ( http://www.linuxnet.com )
+ *
+ * Copyright (C) 1999
+ *  David Corcoran <corcoran@linuxnet.com>
+ * Copyright (C) 2004
+ *  Ludovic Rousseau <ludovic.rousseau@free.fr>
+ *
+ * $Id$
+ */
+
+/**
+ * @file
+ * @brief This handles card insertion/removal events, updates ATR,
+ * protocol, and status information.
+ */
+
+#ifndef __eventhandler_h__
+#define __eventhandler_h__
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+	/**
+	 * Define an exported public reader state structure so each
+	 * application gets instant notification of changes in state.
+	 */
+	typedef struct pubReaderStatesList
+	{
+		int32_t readerID;
+		char readerName[MAX_READERNAME];
+		uint32_t readerState;
+		int32_t readerSharing;
+
+		UCHAR cardAtr[MAX_ATR_SIZE];
+		uint32_t cardAtrLength;
+		uint32_t cardProtocol;
+	}
+	READER_STATE, *PREADER_STATE;
+
+	LONG EHInitializeEventStructures(void);
+	LONG EHSpawnEventHandler(PREADER_CONTEXT, RESPONSECODE (*)(DWORD));
+	LONG EHDestroyEventHandler(PREADER_CONTEXT);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif							/* __eventhandler_h__ */
