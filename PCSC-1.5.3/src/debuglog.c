cc3b9997618b5ef973cee536c3784f9f63241703
rousseau <rousseau@0ce88b0d-b2fd-0310-8134-9614164e65ea>
2009-05-06 14:17:03 +0000

    release 1.5.3
    git-svn-id: svn://anonscm.debian.org/pcsclite/tags/PCSC@3486 0ce88b0d-b2fd-0310-8134-9614164e65ea

---
+++ PCSC-1.5.3/src/debuglog.c
@@ -0,0 +1,301 @@
+/*
+ * MUSCLE SmartCard Development ( http://www.linuxnet.com )
+ *
+ * Copyright (C) 1999-2002
+ *  David Corcoran <corcoran@linuxnet.com>
+ * Copyright (C) 1999-2005
+ *  Ludovic Rousseau <ludovic.rousseau@free.fr>
+ *
+ * $Id$
+ */
+
+/**
+ * @file
+ * @brief This handles debugging for pcscd.
+ */
+
+#include "config.h"
+#ifdef HAVE_SYSLOG_H
+#include <syslog.h>
+#endif
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <time.h>
+
+#include "pcsclite.h"
+#include "misc.h"
+#include "debuglog.h"
+#include "sys_generic.h"
+#include "strlcpycat.h"
+
+/**
+ * Max string size when dumping a 256 bytes longs APDU
+ * Should be bigger than 256*3+30
+ */
+#define DEBUG_BUF_SIZE 2048
+
+static char LogSuppress = DEBUGLOG_LOG_ENTRIES;
+static char LogMsgType = DEBUGLOG_NO_DEBUG;
+static char LogCategory = DEBUG_CATEGORY_NOTHING;
+
+/** default level */
+static char LogLevel = PCSC_LOG_ERROR;
+
+static signed char LogDoColor = 0;	/**< no color by default */
+
+static void log_line(const int priority, const char *DebugBuffer);
+
+void log_msg(const int priority, const char *fmt, ...)
+{
+	char DebugBuffer[DEBUG_BUF_SIZE];
+	va_list argptr;
+
+	if ((LogSuppress != DEBUGLOG_LOG_ENTRIES)
+		|| (priority < LogLevel) /* log priority lower than threshold? */
+		|| (DEBUGLOG_NO_DEBUG == LogMsgType))
+		return;
+
+	va_start(argptr, fmt);
+	vsnprintf(DebugBuffer, DEBUG_BUF_SIZE, fmt, argptr);
+	va_end(argptr);
+
+	log_line(priority, DebugBuffer);
+} /* log_msg */
+
+static void log_line(const int priority, const char *DebugBuffer)
+{
+	if (DEBUGLOG_SYSLOG_DEBUG == LogMsgType)
+		syslog(LOG_INFO, "%s", DebugBuffer);
+	else
+	{
+		if (LogDoColor)
+		{
+			const char *color_pfx = "", *color_sfx = "\33[0m";
+			const char *time_pfx = "\33[36m", *time_sfx = color_sfx;
+			static struct timeval last_time = { 0, 0 };
+			struct timeval new_time = { 0, 0 };
+			struct timeval tmp;
+			int delta;
+
+			switch (priority)
+			{
+				case PCSC_LOG_CRITICAL:
+					color_pfx = "\33[01;31m"; /* bright + Red */
+					break;
+
+				case PCSC_LOG_ERROR:
+					color_pfx = "\33[35m"; /* Magenta */
+					break;
+
+				case PCSC_LOG_INFO:
+					color_pfx = "\33[34m"; /* Blue */
+					break;
+
+				case PCSC_LOG_DEBUG:
+					color_pfx = ""; /* normal (black) */
+					color_sfx = "";
+					break;
+			}
+
+			gettimeofday(&new_time, NULL);
+			if (0 == last_time.tv_sec)
+				last_time = new_time;
+
+			tmp.tv_sec = new_time.tv_sec - last_time.tv_sec;
+			tmp.tv_usec = new_time.tv_usec - last_time.tv_usec;
+			if (tmp.tv_usec < 0)
+			{
+				tmp.tv_sec--;
+				tmp.tv_usec += 1000000;
+			}
+			if (tmp.tv_sec < 100)
+				delta = tmp.tv_sec * 1000000 + tmp.tv_usec;
+			else
+				delta = 99999999;
+
+			fprintf(stderr, "%s%.8d%s %s%s%s\n", time_pfx, delta, time_sfx,
+				color_pfx, DebugBuffer, color_sfx);
+			last_time = new_time;
+		}
+		else
+			fprintf(stderr, "%s\n", DebugBuffer);
+	}
+} /* log_msg */
+
+void log_xxd(const int priority, const char *msg, const unsigned char *buffer,
+	const int len)
+{
+	char DebugBuffer[DEBUG_BUF_SIZE];
+	int i;
+	char *c;
+	char *debug_buf_end;
+
+	if ((LogSuppress != DEBUGLOG_LOG_ENTRIES)
+		|| (priority < LogLevel) /* log priority lower than threshold? */
+		|| (DEBUGLOG_NO_DEBUG == LogMsgType))
+		return;
+
+	debug_buf_end = DebugBuffer + DEBUG_BUF_SIZE - 5;
+
+	strlcpy(DebugBuffer, msg, sizeof(DebugBuffer));
+	c = DebugBuffer + strlen(DebugBuffer);
+
+	for (i = 0; (i < len) && (c < debug_buf_end); ++i)
+	{
+		sprintf(c, "%02X ", buffer[i]);
+		c += 3;
+	}
+
+	/* the buffer is too small so end it with "..." */
+	if ((c >= debug_buf_end) && (i < len))
+		c[-3] = c[-2] = c[-1] = '.';
+
+	log_line(priority, DebugBuffer);
+} /* log_xxd */
+
+#ifdef PCSCD
+void DebugLogSuppress(const int lSType)
+{
+	LogSuppress = lSType;
+}
+#endif
+
+void DebugLogSetLogType(const int dbgtype)
+{
+	switch (dbgtype)
+	{
+		case DEBUGLOG_NO_DEBUG:
+		case DEBUGLOG_SYSLOG_DEBUG:
+		case DEBUGLOG_STDERR_DEBUG:
+			LogMsgType = dbgtype;
+			break;
+		default:
+			Log2(PCSC_LOG_CRITICAL, "unknown log type (%d), using stderr",
+				dbgtype);
+			LogMsgType = DEBUGLOG_STDERR_DEBUG;
+	}
+
+	/* log to stderr and stderr is a tty? */
+	if (DEBUGLOG_STDERR_DEBUG == LogMsgType && isatty(fileno(stderr)))
+	{
+		const char *terms[] = { "linux", "xterm", "xterm-color", "Eterm", "rxvt", "rxvt-unicode" };
+		char *term;
+
+		term = getenv("TERM");
+		if (term)
+		{
+			unsigned int i;
+
+			/* for each known color terminal */
+			for (i = 0; i < sizeof(terms) / sizeof(terms[0]); i++)
+			{
+				/* we found a supported term? */
+				if (0 == strcmp(terms[i], term))
+				{
+					LogDoColor = 1;
+					break;
+				}
+			}
+		}
+	}
+}
+
+void DebugLogSetLevel(const int level)
+{
+	LogLevel = level;
+	switch (level)
+	{
+		case PCSC_LOG_CRITICAL:
+		case PCSC_LOG_ERROR:
+			/* do not log anything */
+			break;
+
+		case PCSC_LOG_INFO:
+			Log1(PCSC_LOG_INFO, "debug level=notice");
+			break;
+
+		case PCSC_LOG_DEBUG:
+			Log1(PCSC_LOG_DEBUG, "debug level=debug");
+			break;
+
+		default:
+			LogLevel = PCSC_LOG_INFO;
+			Log2(PCSC_LOG_CRITICAL, "unknown level (%d), using level=notice",
+				level);
+	}
+}
+
+INTERNAL int DebugLogSetCategory(const int dbginfo)
+{
+#define DEBUG_INFO_LENGTH 80
+	char text[DEBUG_INFO_LENGTH];
+
+	/* use a negative number to UNset
+	 * typically use ~DEBUG_CATEGORY_APDU
+	 */
+	if (dbginfo < 0)
+		LogCategory &= dbginfo;
+	else
+		LogCategory |= dbginfo;
+
+	/* set to empty string */
+	text[0] = '\0';
+
+	if (LogCategory & DEBUG_CATEGORY_APDU)
+		strlcat(text, " APDU", sizeof(text));
+
+	Log2(PCSC_LOG_INFO, "Debug options:%s", text);
+
+	return LogCategory;
+}
+
+INTERNAL void DebugLogCategory(const int category, const unsigned char *buffer,
+	const int len)
+{
+	if ((category & DEBUG_CATEGORY_APDU)
+		&& (LogCategory & DEBUG_CATEGORY_APDU))
+		log_xxd(PCSC_LOG_INFO, "APDU: ", (const unsigned char *)buffer, len);
+
+	if ((category & DEBUG_CATEGORY_SW)
+		&& (LogCategory & DEBUG_CATEGORY_APDU))
+		log_xxd(PCSC_LOG_INFO, "SW: ", (const unsigned char *)buffer, len);
+}
+
+/*
+ * old function supported for backward object code compatibility
+ * defined only for pcscd
+ */
+#ifdef PCSCD
+void debug_msg(const char *fmt, ...);
+void debug_msg(const char *fmt, ...)
+{
+	char DebugBuffer[DEBUG_BUF_SIZE];
+	va_list argptr;
+
+	if ((LogSuppress != DEBUGLOG_LOG_ENTRIES)
+		|| (DEBUGLOG_NO_DEBUG == LogMsgType))
+		return;
+
+	va_start(argptr, fmt);
+	vsnprintf(DebugBuffer, DEBUG_BUF_SIZE, fmt, argptr);
+	va_end(argptr);
+
+	if (DEBUGLOG_SYSLOG_DEBUG == LogMsgType)
+		syslog(LOG_INFO, "%s", DebugBuffer);
+	else
+		fprintf(stderr, "%s\n", DebugBuffer);
+} /* debug_msg */
+
+void debug_xxd(const char *msg, const unsigned char *buffer, const int len);
+void debug_xxd(const char *msg, const unsigned char *buffer, const int len)
+{
+	log_xxd(PCSC_LOG_ERROR, msg, buffer, len);
+} /* debug_xxd */
+#endif
+
---
+++ PCSC-1.5.3/src/readerfactory.h
@@ -0,0 +1,180 @@
+/*
+ * MUSCLE SmartCard Development ( http://www.linuxnet.com )
+ *
+ * Copyright (C) 1999
+ *  David Corcoran <corcoran@linuxnet.com>
+ * Copyright (C) 2004
+ *  Ludovic Rousseau <ludovic.rousseau@free.fr>
+ *
+ * $Id$
+ */
+
+/**
+ * @file
+ * @brief This keeps track of a list of currently available reader structures.
+ */
+
+#ifndef __readerfactory_h__
+#define __readerfactory_h__
+
+#include <inttypes.h>
+
+#include "thread_generic.h"
+#include "ifdhandler.h"
+#include "pcscd.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+	typedef struct
+	{
+		char *pcFriendlyname;	/**< FRIENDLYNAME */
+		char *pcDevicename;		/**< DEVICENAME */
+		char *pcLibpath;		/**< LIBPATH */
+		int dwChannelId;		/**< CHANNELID */
+	} SerialReader;
+
+	struct FctMap_V1
+	{
+		RESPONSECODE (*pvfCreateChannel)(DWORD);
+		RESPONSECODE (*pvfCloseChannel)(void);
+		RESPONSECODE (*pvfGetCapabilities)(DWORD, PUCHAR);
+		RESPONSECODE (*pvfSetCapabilities)(DWORD, PUCHAR);
+		RESPONSECODE (*pvfSetProtocolParameters)(DWORD, UCHAR, UCHAR, UCHAR,
+			UCHAR);
+		RESPONSECODE (*pvfPowerICC)(DWORD);
+		RESPONSECODE (*pvfTransmitToICC)(SCARD_IO_HEADER, PUCHAR, DWORD,
+			PUCHAR, PDWORD, PSCARD_IO_HEADER);
+		RESPONSECODE (*pvfICCPresence)(void);
+	};
+
+	typedef struct FctMap_V1 FCT_MAP_V1, *PFCT_MAP_V1;
+
+	struct FctMap_V2
+	{
+		/* shared with API 3.0 */
+		RESPONSECODE (*pvfCreateChannel)(DWORD, DWORD);
+		RESPONSECODE (*pvfCloseChannel)(DWORD);
+		RESPONSECODE (*pvfGetCapabilities)(DWORD, DWORD, PDWORD, PUCHAR);
+		RESPONSECODE (*pvfSetCapabilities)(DWORD, DWORD, DWORD, PUCHAR);
+		RESPONSECODE (*pvfSetProtocolParameters)(DWORD, DWORD, UCHAR, UCHAR,
+			UCHAR, UCHAR);
+		RESPONSECODE (*pvfPowerICC)(DWORD, DWORD, PUCHAR, PDWORD);
+		RESPONSECODE (*pvfTransmitToICC)(DWORD, SCARD_IO_HEADER, PUCHAR,
+			DWORD, PUCHAR, PDWORD, PSCARD_IO_HEADER);
+		RESPONSECODE (*pvfICCPresence)(DWORD);
+
+		/* API v2.0 only */
+		RESPONSECODE (*pvfControl)(DWORD, PUCHAR, DWORD, PUCHAR, PDWORD);
+	};
+
+	typedef struct FctMap_V2 FCT_MAP_V2, *PFCT_MAP_V2;
+
+	struct FctMap_V3
+	{
+		/* the common fields SHALL be in the same order as in FctMap_V2 */
+		RESPONSECODE (*pvfCreateChannel)(DWORD, DWORD);
+		RESPONSECODE (*pvfCloseChannel)(DWORD);
+		RESPONSECODE (*pvfGetCapabilities)(DWORD, DWORD, PDWORD, PUCHAR);
+		RESPONSECODE (*pvfSetCapabilities)(DWORD, DWORD, DWORD, PUCHAR);
+		RESPONSECODE (*pvfSetProtocolParameters)(DWORD, DWORD, UCHAR, UCHAR,
+				UCHAR, UCHAR);
+		RESPONSECODE (*pvfPowerICC)(DWORD, DWORD, PUCHAR, PDWORD);
+		RESPONSECODE (*pvfTransmitToICC)(DWORD, SCARD_IO_HEADER, PUCHAR,
+			DWORD, PUCHAR, PDWORD, PSCARD_IO_HEADER);
+		RESPONSECODE (*pvfICCPresence)(DWORD);
+
+		/* API V3.0 only */
+		RESPONSECODE (*pvfControl)(DWORD, DWORD, LPCVOID, DWORD, LPVOID,
+			DWORD, LPDWORD);
+		RESPONSECODE (*pvfCreateChannelByName)(DWORD, LPSTR);
+	};
+
+	typedef struct FctMap_V3 FCT_MAP_V3, *PFCT_MAP_V3;
+
+	struct RdrCliHandles
+	{
+		SCARDHANDLE hCard;		/**< hCard for this connection */
+		DWORD dwEventStatus;	/**< Recent event that must be sent */
+	};
+
+	typedef struct RdrCliHandles RDR_CLIHANDLES, *PRDR_CLIHANDLES;
+
+	struct ReaderContext
+	{
+		char lpcReader[MAX_READERNAME];	/**< Reader Name */
+		char lpcLibrary[MAX_LIBNAME];	/**< Library Path */
+		char lpcDevice[MAX_DEVICENAME];	/**< Device Name */
+		PCSCLITE_THREAD_T pthThread;	/**< Event polling thread */
+		RESPONSECODE (*pthCardEvent)(DWORD);	/**< Card Event sync */
+		PCSCLITE_MUTEX_T mMutex;	/**< Mutex for this connection */
+		RDR_CLIHANDLES psHandles[PCSCLITE_MAX_READER_CONTEXT_CHANNELS];
+                                         /**< Structure of connected handles */
+		union
+		{
+			FCT_MAP_V1 psFunctions_v1;	/**< API V1.0 */
+			FCT_MAP_V2 psFunctions_v2;	/**< API V2.0 */
+			FCT_MAP_V3 psFunctions_v3;	/**< API V3.0 */
+		} psFunctions;	/**< driver functions */
+
+		LPVOID vHandle;			/**< Dlopen handle */
+		DWORD dwVersion;		/**< IFD Handler version number */
+		DWORD dwPort;			/**< Port ID */
+		DWORD dwSlot;			/**< Current Reader Slot */
+		DWORD dwBlockStatus;	/**< Current blocking status */
+		DWORD dwLockId;			/**< Lock Id */
+		int LockCount;			/**< number of recursive locks */
+		DWORD dwIdentity;		/**< Shared ID High Nibble */
+		int32_t dwContexts;		/**< Number of open contexts */
+		PDWORD pdwFeeds;		/**< Number of shared client to lib */
+		PDWORD pdwMutex;		/**< Number of client to mutex */
+
+		struct pubReaderStatesList *readerState; /**< link to the reader state */
+		/* we can't use PREADER_STATE here since eventhandler.h can't be
+		 * included because of circular dependencies */
+	};
+
+	typedef struct ReaderContext READER_CONTEXT, *PREADER_CONTEXT;
+
+	LONG RFAllocateReaderSpace(void);
+	LONG RFAddReader(LPSTR, DWORD, LPSTR, LPSTR);
+	LONG RFRemoveReader(LPSTR, DWORD);
+	LONG RFSetReaderName(PREADER_CONTEXT, LPSTR, LPSTR, DWORD, DWORD);
+	LONG RFListReaders(LPSTR, LPDWORD);
+	LONG RFReaderInfo(LPSTR, /*@out@*/ struct ReaderContext **);
+	LONG RFReaderInfoNamePort(DWORD, LPSTR, /*@out@*/ struct ReaderContext **);
+	LONG RFReaderInfoById(DWORD, /*@out@*/ struct ReaderContext **);
+	LONG RFCheckSharing(DWORD);
+	LONG RFLockSharing(DWORD);
+	LONG RFUnlockSharing(DWORD);
+	LONG RFUnlockAllSharing(DWORD);
+	LONG RFUnblockReader(PREADER_CONTEXT);
+	LONG RFUnblockContext(SCARDCONTEXT);
+	LONG RFLoadReader(PREADER_CONTEXT);
+	LONG RFBindFunctions(PREADER_CONTEXT);
+	LONG RFUnBindFunctions(PREADER_CONTEXT);
+	LONG RFUnloadReader(PREADER_CONTEXT);
+	LONG RFInitializeReader(PREADER_CONTEXT);
+	LONG RFUnInitializeReader(PREADER_CONTEXT);
+	SCARDHANDLE RFCreateReaderHandle(PREADER_CONTEXT);
+	LONG RFDestroyReaderHandle(SCARDHANDLE hCard);
+	LONG RFAddReaderHandle(PREADER_CONTEXT, SCARDHANDLE);
+	LONG RFFindReaderHandle(SCARDHANDLE);
+	LONG RFRemoveReaderHandle(PREADER_CONTEXT, SCARDHANDLE);
+	LONG RFSetReaderEventState(PREADER_CONTEXT, DWORD);
+	LONG RFCheckReaderEventState(PREADER_CONTEXT, SCARDHANDLE);
+	LONG RFClearReaderEventState(PREADER_CONTEXT, SCARDHANDLE);
+	LONG RFCheckReaderStatus(PREADER_CONTEXT);
+	void RFCleanupReaders(int);
+	int RFStartSerialReaders(const char *readerconf);
+	void RFReCheckReaderConf(void);
+	void RFSuspendAllReaders(void);
+	void RFAwakeAllReaders(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
