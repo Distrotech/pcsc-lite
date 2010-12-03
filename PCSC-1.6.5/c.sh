0dc96e7a8ef24131bc5b199d302542b89633f900
rousseau <rousseau@0ce88b0d-b2fd-0310-8134-9614164e65ea>
2010-12-03 14:22:13 +0000

    release 1.6.5
    git-svn-id: svn://anonscm.debian.org/pcsclite/tags/PCSC@5428 0ce88b0d-b2fd-0310-8134-9614164e65ea

---
+++ PCSC-1.6.5/c.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -x
+
+#CFLAGS="$CFLAGS -Wall -g -O2 -Wextra -pipe -funsigned-char -fstrict-aliasing -Wchar-subscripts -Wundef -Wshadow -Wcast-align -Wwrite-strings -Wunused -Wno-unused-value -Wuninitialized -Wpointer-arith -Wredundant-decls -Wmissing-prototypes -Wstrict-prototypes -Wold-style-definition -Wmissing-declarations -Wbad-function-cast -Winline -Wnested-externs -Wformat-security -Wswitch-enum -Winit-self -Wmissing-include-dirs -Wno-unused-parameter -Wno-sign-compare"
+
+./configure \
+        --prefix=/usr \
+        --sysconfdir=/etc \
+        --enable-maintainer-mode \
+        --enable-twinserial \
+        CFLAGS="$CFLAGS" \
+        "$@"
---
+++ PCSC-1.6.5/src/atrhandler.h
@@ -0,0 +1,62 @@
+/*
+ * MUSCLE SmartCard Development ( http://www.linuxnet.com )
+ *
+ * Copyright (C) 1999-2002
+ *  David Corcoran <corcoran@linuxnet.com>
+ * Copyright (C) 2002-2009
+ *  Ludovic Rousseau <ludovic.rousseau@free.fr>
+ *
+ * $Id$
+ */
+
+/**
+ * @file
+ * @brief This keeps track of smartcard protocols, timing issues
+ * and Answer to Reset ATR handling.
+ */
+
+#ifndef __atrhandler_h__
+#define __atrhandler_h__
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#define SCARD_CONVENTION_DIRECT  0x0001
+#define SCARD_CONVENTION_INVERSE 0x0002
+
+	typedef struct
+	{
+
+		struct
+		{
+			int Length;
+			int HistoryLength;
+			UCHAR Value[MAX_ATR_SIZE];
+			UCHAR HistoryValue[MAX_ATR_SIZE];
+		}
+		ATR;
+
+		struct
+		{
+			UCHAR AvailableProtocols;
+			UCHAR CurrentProtocol;
+			UCHAR Convention;
+		}
+		CardCapabilities;
+	}
+	SMARTCARD_EXTENSION;
+
+	/*
+	 * Decodes the ATR and fills the structure
+	 */
+
+	short ATRDecodeAtr(/*@out@*/ SMARTCARD_EXTENSION *psExtension,
+		PUCHAR pucAtr, DWORD dwLength);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif							/* __atrhandler_h__ */
