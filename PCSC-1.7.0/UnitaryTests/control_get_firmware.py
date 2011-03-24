b929972c404bc447ac3c0a8958e0e11ed50cf940
rousseau <rousseau@0ce88b0d-b2fd-0310-8134-9614164e65ea>
2011-03-24 21:50:53 +0000

    PCSC release 1.7.0
    git-svn-id: svn://anonscm.debian.org/pcsclite/tags/PCSC@5668 0ce88b0d-b2fd-0310-8134-9614164e65ea

---
+++ PCSC-1.7.0/UnitaryTests/control_get_firmware.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+
+"""
+#   control_get_firmware.py: get firmware version of Gemalto readers
+#   Copyright (C) 2009-2010  Ludovic Rousseau
+"""
+
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along
+#   with this program; if not, write to the Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+from smartcard.System import readers
+from smartcard.pcsc.PCSCPart10 import (SCARD_SHARE_DIRECT,
+    SCARD_LEAVE_CARD, SCARD_CTL_CODE)
+
+for reader in readers():
+    cardConnection = reader.createConnection()
+    cardConnection.connect(mode=SCARD_SHARE_DIRECT,
+        disposition=SCARD_LEAVE_CARD)
+
+    get_firmware = [0x02]
+    IOCTL_SMARTCARD_VENDOR_IFD_EXCHANGE = SCARD_CTL_CODE(1)
+    res = cardConnection.control(IOCTL_SMARTCARD_VENDOR_IFD_EXCHANGE,
+        get_firmware)
+    print "Reader:", reader
+    print "Firmware:", "".join([chr(x) for x in res])
